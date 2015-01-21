6e3c39a5a47211497f25481ae2fdada120bee8b8
EduardoZola <eduzol@gmail.com>
2015-01-21 01:12:46 -0800

    Add Business logic Create Tests

--- src/sudoku.js
+++ src/sudoku.js
@@ -1,9 +1,140 @@
 var SUDOKU = ( function(){
 	
-	console.log('this is sudoku.js');
+	/**
+	BR1 The board is comprised of a 9×9 matrix, divided into 9 sub sections
+	BR2 Each square can have a number from 1 to 9
+	BR3 Numbers must be unique per row
+	BR4 Numbers must be unique per column
+	BR5 Numbers must be unique per section
+	**/
+	
+	var N = 9;
+	var rows =[];
+	var columns = [];
+	var sections = [];
+	
+	var onSuccesMove = {};
+	var onFailureMove = {};
+	var onCompleted = {};
+	
+	var success = {message: "success" , value : ""};
+	
+	var columnValidationError = {   message :"columnValidationError" , value : "" };
+	var rowValidationError = {   message :"rowValidationError" , value : "" };
+	var sectionValidationError = { message : "sectionValidationError" , value : ""};
+	var invalidNumber = { message : "invalidNumber" , value:""};
+	
+	var checkRow = function ( row, value ){
+		var rowValue = rows[row][value];
+		if ( rowValue){
+			return false;
+		}else{
+			return true;
+		}
+		
+	};
+	
+	var checkColumn = function( column, value ){
+		var columnValue = columns[column][value];
+		if ( columnValue){
+			return false;
+		}else{
+			return true;
+		}
+	}
+	
+	var checkSection = function( section , value ){
+		var sectionValue = sections[section][value];
+		if ( sectionValue){
+			return false;
+		}else{
+			return true;
+		}
+		
+	}
+	
+	var validateLimits  = function ( value ){
+		
+		if ( value > 0  && value <= 9 ){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	for ( var i = 0 ; i < N ; i++){
+		rows[i] = [];
+		columns[i] = [];
+		sections[i] = [];
+	}
 	
 	
+	this.init = function ( config ) {
+		
+		onSuccesMove = config.succesfulMove ;
+		onfailureMove = config.failureMove ;
+		onCompleted = config.completed;
+		
+		
+		
+
+	};
 	
+	this.setMove  = function ( section , column , row  , value ){
+
+		if ( !validateLimits(section )){
+			
+			invalidNumber.value = section;
+			onfailureMove(invalidNumber);
+			return ;
+			
+			
+		}else if ( !validateLimits(column) ){
+			
+			invalidNumber.value = column;
+			onfailureMove(invalidNumber);
+			return ;
+			
+			
+		}else if ( !validateLimits(row)){
+			
+			invalidNumber.value = row;
+			onfailureMove(invalidNumber);
+			return ;
+			
+		}
+		
+		var validRow  = checkRow(  row , value  );
+		if ( validRow ){
+			rows[row][value] = true;
+		}else{
+			rowValidationError.value = value;
+			onfailureMove(rowValidationError);
+			return;
+		}
+		
+		var validColumn = checkColumn( column, value);
+		if ( validColumn ){
+			columns[column][value] =  true;
+		}else{
+			columnValidationError.value = value;
+			onfailureMove(columnValidationError);
+			return;
+		}
+		
+		var validSection =  checkSection(section , value  );
+		if ( validSection ){
+			sections[section][value] = true;
+		}else{
+			sectionValidationError.value = value;
+			onfailureMove(sectionValidationError);	
+			return;
+		}
+		success.value= value;
+		onSuccesMove( success );
+		
+		return;
+	};
 	
-	
+	return this;
 }());
--- test/sudoku-test.js
+++ test/sudoku-test.js
@@ -1,5 +1,69 @@
-QUnit.test( "hello test", function( assert ) {
+/*
+ * 
+ * SUDOKU.init ({
+ * 	
+ *   successfulMove : function() ,
+ *   failureMove : function() , 
+ *   completed : function(), 
+ *   
+ * });
+ * 
+ * SUDOKU.setMove( section , column , row  , value );
+ *  
+ */
+
+QUnit.test( "succesful move", function( assert ) {
  
-	assert.ok( 1 == "1", "Passed!" );
+	var onSucess = function( result ){
+		console.log("onSuccess Test Result = "+ result.message + " value " + result.value  );
+		assert.ok(true , "onSuccess called");
+	};
+	
+	var onFailure = function( result ){
+		console.log("onFailure Test Result = "+ result.message + " value " + result.value  );
+		assert.ok(false, 'onFailure called');
+	};
+	
+	var onCompleted = function( result){
+		console.log("onCompleted Test Result = "+ result.message + " value " + result.value  );
+		assert.ok(false, 'onCompleted called');
+	};
+	
+	SUDOKU.init({
+		succesfulMove : onSucess ,
+		failureMove : onFailure  , 
+		completed : onCompleted 
+	});
+	
+	SUDOKU.setMove( 1 , 1, 1, 3);
+	
+});
+
+
+QUnit.test( "unsuccesful move", function( assert ) {
+	 
+	var onSucess1 = function( result ){
+		console.log("onSuccess1 Test Result = "+ result.message + " value " + result.value  );
+		assert.ok(false , "onSuccess called");
+	};
+	
+	var onFailure1 = function( result ){
+		console.log("onFailure1 Test Result = "+ result.message + " value " + result.value  );
+		assert.ok(true, 'onFailure called');
+	};
+	
+	var onCompleted1 = function( result){
+		console.log("onCompleted1 Test Result = "+ result.message + " value " + result.value  );
+		assert.ok(false, 'onCompleted called');
+	};
+	
+	SUDOKU.init({
+		succesfulMove : onSucess1 ,
+		failureMove : onFailure1  , 
+		completed : onCompleted1 
+	});
+	SUDOKU.setMove( 1 , 1, 2, 3);
+	
+	
 	
 });
